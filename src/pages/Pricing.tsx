838a8fd55b7af8a0f9575de7ec4299040b7d4541
William <williambosworth420@gmail.com>
2026-01-04 22:29:29 +0100

    William: 504 error possible fix 6 - UI position changes

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -225,7 +225,7 @@ const Pricing: React.FC = () => {
         </div>
 
         <div
-          ref={gridRef}
+          ref={gridRef} // ref to the grid container
           data-animate-id="pricing-grid"
           className={`pricing__grid ${
             visibleElements.has("pricing-grid") ? "fade-in" : ""
